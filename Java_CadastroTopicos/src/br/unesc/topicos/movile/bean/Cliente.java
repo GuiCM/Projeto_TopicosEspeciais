1df21a0007e1a9b10987c04ed8e099a4039aa642
comp1 <comp1@comp1_17.lab.unesc.net>
2016-06-17 20:22:27 -0300

    Metodo Insert implementado

--- Java_CadastroTopicos/src/br/unesc/topicos/movile/bean/Cliente.java
+++ Java_CadastroTopicos/src/br/unesc/topicos/movile/bean/Cliente.java
@@ -1,12 +1,14 @@
 package br.unesc.topicos.movile.bean;
 
 public class Cliente {
+
+    private int codigo;
     private String nome;
     private int idade;
     private String dataNascimento;
     private char sexo;
     private String cpf;
-    private String rg; 
+    private String rg;
     private String rua;
     private int numero;
     private String cep;
@@ -16,7 +18,12 @@ public class Cliente {
 
     /**
      * ***** GET'S ******
+     * @return 
      */
+    public int getCodigo() {
+        return this.codigo;
+    }
+
     public String getNome() {
         return this.nome;
     }
@@ -40,19 +47,19 @@ public class Cliente {
     public String getRg() {
         return this.rg;
     }
-    
+
     public String getRua() {
         return this.rua;
     }
-    
+
     public int getNumero() {
         return this.numero;
     }
-    
+
     public String getCep() {
         return this.cep;
     }
-    
+
     public String getCidade() {
         return this.cidade;
     }
@@ -60,12 +67,18 @@ public class Cliente {
     public String getEstado() {
         return this.estado;
     }
-    
+
     public String getBairro() {
         return this.bairro;
     }
-    
-    /******* SET'S *******/
+
+    /**
+     * ***** SET'S ******
+     */
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
@@ -89,27 +102,27 @@ public class Cliente {
     public void setRg(String rg) {
         this.rg = rg;
     }
-    
+
     public void setRua(String rua) {
         this.rua = rua;
     }
-    
+
     public void setNumero(int numero) {
         this.numero = numero;
     }
-    
+
     public void setCep(String cep) {
         this.cep = cep;
     }
-    
+
     public void setCidade(String cidade) {
         this.cidade = cidade;
     }
-    
+
     public void setEstado(String estado) {
         this.estado = estado;
     }
-    
+
     public void setBairro(String bairro) {
         this.bairro = bairro;
     }
--- Java_CadastroTopicos/src/br/unesc/topicos/movile/controle/ClienteDAO.java
+++ Java_CadastroTopicos/src/br/unesc/topicos/movile/controle/ClienteDAO.java
@@ -248,4 +248,73 @@ public class ClienteDAO {
         return null;
     }
 
+    public void update(Cliente cliente) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = Conexao.getConnection();
+            
+            String sql = "update produtos set "
+                    + "nome = ?,"
+                    + "idade = ?,"
+                    + "dataNascimento = ?,"
+                    + "char = ?,"
+                    + "sexo = ?,"
+                    + "cpf = ?,"
+                    + "rg = ?,"
+                    + "rua = ?,"
+                    + "numero = ?,"
+                    + "cep = ?,"
+                    + "cidade = ?,"
+                    + "estado = ?,"
+                    + "bairro = ?"
+                    + "where codigo = ?";
+            
+            ps = conn.prepareStatement(sql);
+
+            ps.setString(1, cliente.getNome());
+            ps.setInt(2, cliente.getIdade());
+            ps.setString(3, cliente.getDataNascimento());
+            ps.setString(4, String.valueOf(cliente.getSexo()));
+            ps.setString(5, cliente.getCpf());
+            ps.setString(6, cliente.getRg());
+            ps.setString(7, cliente.getRua());
+            ps.setInt(8, cliente.getNumero());
+            ps.setString(9, cliente.getCep());
+            ps.setString(10, cliente.getCidade());
+            ps.setString(11, cliente.getEstado());
+            ps.setString(12, cliente.getBairro());
+
+            ps.execute();
+
+            conn.commit();
+        } catch (SQLException e) {
+            System.out.println("ERRO: " + e.getMessage());
+
+            if (conn != null) {
+                try {
+                    conn.rollback();
+                } catch (SQLException ex) {
+                    System.out.println("ERRO: " + ex.getMessage());
+                }
+            }
+
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    System.out.println("ERRO: " + ex.getMessage());
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    System.out.println("ERRO: " + ex.getMessage());
+                }
+            }
+        }
+    }
+
 }
